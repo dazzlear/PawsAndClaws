43d41edab3a98ef653760ca933c88d0b58ff8320
dazzlear <dazel.argallon7@gmail.com>
2025-12-29 21:57:05 +0800

    Add project files.

---
+++ PawsAndClaws/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PawsAndClaws.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
