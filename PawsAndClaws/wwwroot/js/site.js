b264faa1d1522538d9db72b3f23e1219ff6140f9
Julianna Leila Castillo <juliannaleilacastillo@gmail.com>
2026-01-05 22:48:19 +0800

    feat: completed applications page

--- PawsAndClaws/wwwroot/js/site.js
+++ PawsAndClaws/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+  // Dropdown Toggle Logic
+  $(document).on("click", ".dropdown-toggle", function (e) {
+    e.stopPropagation();
+    const menu = $(this).next(".dropdown-menu");
+
+    // Close other open menus
+    $(".dropdown-menu").not(menu).addClass("hidden");
+
+    // Toggle current menu
+    menu.toggleClass("hidden");
+  });
+
+  // Close dropdowns when clicking outside
+  $(document).on("click", function () {
+    $(".dropdown-menu").addClass("hidden");
+  });
+
+  // Prevent menu closure when clicking inside the menu
+  $(document).on("click", ".dropdown-menu", function (e) {
+    e.stopPropagation();
+  });
+});
